101a785e1cea3dea0d5c1f1342ae8e09cdefa5f0
Oleksandr <sashabohutskiy@gmail.com>
2016-09-14 00:52:30 +0300

    Added 2 tests

---
+++ src/main/java/Lab1/CONST.java
@@ -0,0 +1,7 @@
+package Lab1;
+
+/**
+ * Created by Олександр on 13.09.2016.
+ */
+public class CONST {
+}
---
+++ src/main/java/Lab1/criterion/Test1.java
@@ -0,0 +1,7 @@
+package Lab1.criterion;
+
+/**
+ * Created by Олександр on 13.09.2016.
+ */
+public class Test1 {
+}
